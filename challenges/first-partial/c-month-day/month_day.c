81cdc0abc14d8e77a544b6c22f0702f317c09bd4
A01634706 <A01634706@itesm.mx>
2021-09-09 04:30:41 +0000

    ExamenP1

--- challenges/first-partial/c-month-day/month_day.c
+++ challenges/first-partial/c-month-day/month_day.c
@@ -1,8 +1,54 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>  
 /* month_day function's prototype*/
-void month_day(int year, int yearday, int *pmonth, int *pday);
+int month_day(int year, int yearday, int *pmonth, int *pday){
 
-int main() {
+    static char daytab[2][12] = {
+        {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+        {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+    };
+    int i, leap, mesesTot;
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    mesesTot = daytab[leap][1];
+
+    if(leap == 0){
+        if (yearday < 1 || yearday > 365){
+            printf("Error. \n");
+            printf("Introduce un numero del 1 al 366 \n");
+            return 1;
+        }
+    }else {
+        if (yearday < 1 || yearday > 366){
+            printf("Error \n");
+            printf("Introduce un numero del 1 al 366 \n");
+            return 1;
+        }
+    }
+    for (i = 0; i < strlen(daytab[leap]); i++)
+    {
+        if (yearday <= daytab[leap][i]){
+            *pday = yearday;
+            *pmonth = i;
+            return 1;
+        }else{
+            yearday -= daytab[leap][i];
+        }
+    }
+};
+
+int main(int argc, char **argv) {
+    int dia, mes;
+    static char *name[] = {
+       "January", "February", "March",
+       "April", "May", "June",
+       "July", "August", "September",
+       "October", "November", "December"
+    };
+    month_day(atoi(argv[1]), atoi(argv[2]), &mes, &dia);
+
+    if(dia != 0){
+        printf("%s %d, %d \n", name[mes], dia, atoi(argv[1]));
+    }
     return 0;
 }
--- challenges/first-partial/go-functions-methods/geometry.go
+++ challenges/first-partial/go-functions-methods/geometry.go
@@ -1,4 +1,4 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// Copyright ©️ 2016 Alan A. A. Donovan & Brian W. Kernighan.
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 
 // See page 156.
@@ -7,18 +7,83 @@
 //!+point
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+)
 
-type Point struct{ X, Y float64 }
+type Point struct{ x, y float64 }
 
 // traditional function
 func Distance(p, q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Hypot(q.x-p.x, q.y-p.y)
 }
 
 // same thing, but as a method of the Point type
 func (p Point) Distance(q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Hypot(q.x-p.x, q.y-p.y)
+}
+
+func (p Point) X() float64{
+	return p.x
+}
+
+func (p Point) Y() float64{
+	return p.y
+}
+//Retrieved from https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+func onSegment(p, q, r Point) bool{
+	if(q.X() <= math.Max(p.X(), r.X()) && q.X() >= math.Min(p.X(), r.X()) &&
+	q.y <= math.Max(p.y, r.y) && q.y >= math.Min(p.y, r.y)){
+		return true
+	}
+	return false
+}
+
+func orientation(p, q, r Point) int{
+	val := (q.Y() - p.Y()) * (r.X() - q.X()) - (q.X() - p.X()) * (r.Y() - q.Y())
+
+	if (val == 0){
+		return 0
+	}
+
+	if (val > 0){
+		return 1
+	}else {
+		return 2
+	}
+}
+
+func doIntersect(p1, q1, p2, q2 Point) bool {
+	o1 := orientation(p1, q1, p2)
+	o2 := orientation(p1, q1, q2)
+	o3 := orientation(p2, q2, p1)
+	o4 := orientation(p2, q2, q1)
+
+	if (o1 != o2 && o3 != o4){
+		return true
+	}
+
+	if (o1 == 0 && onSegment(p1, p2, q1)){
+		return true
+	}
+
+	if (o2 == 0 && onSegment(p1, q2, q1)){
+		return true
+	}
+
+	if (o3 == 0 && onSegment(p2, p1, q2)){
+		return true
+	}
+
+	if (o4 == 0 && onSegment(p2, q1, q2)){
+		return true
+	}
+
+	return false
 }
 
 //!-point
@@ -40,6 +105,30 @@ func (path Path) Distance() float64 {
 }
 
 func main() {
+	if(len(os.Args) < 2){
+		fmt.Println("Error. Faltan argumentos")
+		return
+	}
+	var arr Path;
+	sides, _ := strconv.Atoi(os.Args[1])
+	for i := 0; i < sides; i++ {
+		p := Point{(rand.Float64()*-100) + 100, (rand.Float64()*-100) + 100}
+		if (i==sides-1) {
+			for j := 0; j<10; j++{
+				if (doIntersect(arr[len(arr)-2],arr[len(arr)-1],arr[len(arr)-1],p)){
+					p = Point{(rand.Float64()*-100) + 100, (rand.Float64()*-100) + 100}
+				}else{
+					break
+				}
+			}
+		} 
+		arr = append(arr, p)
+	}
+
+	arr = append(arr, arr[0])
+
+	fmt.Println("Perimetro: ")
+	fmt.Println(arr.Distance())
 }
 
 //!-path
